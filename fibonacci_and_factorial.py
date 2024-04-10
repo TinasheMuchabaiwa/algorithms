abf67a6b0abf1b783219a61c9bc828862b34c29d
TinasheMuchabaiwa <muchabaiwatinashe@gmail.com>
2024-04-10 15:40:24 +0200

    factorial

--- fibonacci_and_factorial.py
+++ fibonacci_and_factorial.py
@@ -37,3 +37,43 @@ def fibonacci_memo(n, memo={}):
         return 1
     memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
     return memo[n]
+
+
+'''
+PROBLEM DESCRIPTION:
+
+Use the factorial function to find the factorial of a number.
+'''
+
+# solution 1 - using recursion (Time complexity: O(n))
+'''The idea is to use recursion to find the factorial of a number.
+    The base case is when n is 0 or 1, return 1.
+    Otherwise, return the product of n and the factorial of n-1.
+'''
+
+
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    if n == 2:
+        return 2
+
+    return n*factorial(n-1)
+
+
+# solution 2 - using memoization (Time complexity: O(n))
+'''The idea is to use memoization to store the values of the factorial.
+    The base case is when n is 0 or 1, return 1.
+    Otherwise, check if the value of n is already stored in the memo dict.
+    If it is, return the value.
+    If it is not, calculate the value and store it in the memo dict.
+'''
+
+
+def factorial_memo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 1 or n == 0:
+        return 1
+    memo[n] = n*factorial_memo(n-1, memo)
+    return memo[n]
